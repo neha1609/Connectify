a4cb8a0d00387a9e8c87f33d83777d8c02ccb29e
neha1609 <nehajhawar1609@gmail.com>
2021-01-29 11:23:15 +0530

    Customer Address Form added

---
+++ Back end/customer_address.php
@@ -0,0 +1,142 @@
+<?php
+	include("includes/dbcon.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  	<link rel="stylesheet" href="styles/style.css">
+  	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Overpass:wght@900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;800&family=Ubuntu&display=swap" rel="stylesheet">
+	<link href="styles/bootstrap.min.css" rel="stylesheet">
+
+	<link href="styles/style12.css" rel="stylesheet">
+
+
+	<title></title>
+</head>
+<body>
+	<?php
+    	include("includes/header.php");
+  	?>
+
+
+
+  	<div class="row"><!-- 1 row Starts -->
+
+		<div class="col-lg-12"><!-- col-lg-12 Starts -->
+
+			<ol class="breadcrumb"><!-- breadcrumb Starts -->
+
+				<li>
+
+					<i class="fa fa-dashboard"></i> Address of Delivery
+
+				</li>
+
+			</ol><!-- breadcrumb Ends -->
+
+		</div><!-- col-lg-12 Ends -->
+
+	</div><!-- 1 row Ends -->
+
+
+<div class="row"><!-- 2 row Starts -->
+
+<div class="col-lg-12"><!-- col-lg-12 Starts -->
+
+
+	<div class="panel panel-default"><!-- panel panel-default Starts -->
+
+		<div class="panel-heading"><!-- panel-heading Starts -->
+
+			<h3 class="panel-title"><!-- panel-title Starts -->
+
+				<i class="fa fa-money fa-fw"></i> Address
+
+			</h3><!-- panel-title Ends -->
+
+		</div><!-- panel-heading Ends -->
+
+		<div class="panel-body"><!-- panel-body Starts -->
+
+			<form class="form-horizontal" action="" method="post" enctype="multipart/form-data"><!-- form-horizontal Starts -->
+
+				<div class="form-group"><!-- form-group Starts -->
+
+					<label class="col-md-3 control-label">Address</label>
+
+					<div class="col-md-6">
+
+						<input type="text" name="address" class="form-control">
+
+					</div>
+
+				</div><!-- form-group Ends -->
+
+			<div class="form-group"><!-- form-group Starts -->
+
+				<label class="col-md-3 control-label">Pincode</label>
+
+				<div class="col-md-6">
+
+					<input type="text" name="pincode" class="form-control">
+
+				</div>
+
+			</div><!-- form-group Ends -->
+			
+			<div class="form-group"><!-- form-group Starts -->
+
+				<label class="col-md-3 control-label"></label>
+
+				<div class="col-md-6">
+
+					<input type="submit" name="submit" value="Add Customer" class="btn btn-primary form-control">
+
+				</div>
+
+			</div><!-- form-group Ends -->
+
+			</form><!-- form-horizontal Ends -->
+
+		</div><!-- panel-body Ends -->
+
+	</div><!-- panel panel-default Ends -->
+</div>
+</div>
+
+</body>
+</html>
+	<?php
+
+	if(isset($_POST['submit'])){
+
+		$address = $_POST['address'];
+
+		$pincode = $_POST['pincode'];
+		$id = $_GET['id'];
+
+		$insert_cust = "insert into customers (name,city,image,country,email,contact,password) values ('$name','$city','$image1','$country','$email','$contact','$password')";
+
+		$run_cust = mysqli_query($con,$insert_cust);
+
+		if($run_cust){
+
+			echo "<script> alert('New Category Has Been Inserted')</script>";
+
+			echo "<script> window.open('index.php?view_customers','_self') </script>";
+
+		}
+
+	}
+
+
+
+	?>
+
+	<?php 
+		include("includes/footer.php");
+	?>
