52855166218be45b142e976101ab1f167d70c977
Hemangee De <73026143+hemangeede@users.noreply.github.com>
2021-02-10 15:16:24 +0530

    Comment Added

--- Back end/admin/send_mail_d.php
+++ Back end/admin/send_mail_d.php
@@ -15,10 +15,8 @@ if(isset($_POST['submit'] )) {
 			$email = $row['email'];
 			$name = $row['name'];
 		    $message = $_POST['message'];
-		    $from      = 'dpkr5365@gmail.com';
+		    $from      = <Your_Gmail_ID>;
 		    $subject = 'Admin contact with delivery agent trial. :)';
-		    //$headers = 'From: '.$email . "\r\n" .
-		        //"CC: nehajhawar1609@gmail.com";
 
 			$mail = new PHPMailer();
 			$mail->IsSMTP();
@@ -29,14 +27,13 @@ if(isset($_POST['submit'] )) {
 			$mail->SMTPSecure = "tls";
 			$mail->Port       = 587;
 			$mail->Host       = "smtp.gmail.com";
-			$mail->Username   = "dpkr5365@gmail.com";
-			$mail->Password   = "Helloworld#123";
+			$mail->Username   = <Your_Gmail_ID>;
+			$mail->Password   = <Your_Gmail_Password>;
 
 			$mail->IsHTML(true);
-			//$mail->AddAddress("hemangee4700@gmail.com", "Hemangee De");
-			//$mail->SetFrom("dpkr5365@gmail.com", "Deepak Kumar");
+			
 			$mail->AddAddress($email, $name);
-			$mail->SetFrom($from, "Deepak Kumar");
+			$mail->SetFrom($from, <Admin Name>);
 			//$mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
 			//$mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");
 			$mail->Subject = $subject;
