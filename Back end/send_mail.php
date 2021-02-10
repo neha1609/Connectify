4717f025bb4909d889a4c3fd5ee323b77ba59173
Hemangee De <73026143+hemangeede@users.noreply.github.com>
2021-02-10 15:25:14 +0530

    Comment Added

--- Back end/send_mail.php
+++ Back end/send_mail.php
@@ -1,3 +1,5 @@
+<!-- Send Email Function in php -->
+
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -9,10 +11,9 @@ if(isset($_POST['submit'])) {
     $email = $_POST['email'];
     $name = $_POST['name'];
     $message = $_POST['message'];
-    $to      = 'dpkr5365@gmail.com';
+    $to      = <Admin_Gmail_id>;
     $subject = 'Mail from '.$name;
-    //$headers = 'From: '.$email . "\r\n" .
-        //"CC: nehajhawar1609@gmail.com";
+    
 
 	$mail = new PHPMailer();
 	$mail->IsSMTP();
@@ -23,16 +24,14 @@ if(isset($_POST['submit'])) {
 	$mail->SMTPSecure = "tls";
 	$mail->Port       = 587;
 	$mail->Host       = "smtp.gmail.com";
-	$mail->Username   = "dpkr5365@gmail.com";
-	$mail->Password   = "Helloworld#123";
+	$mail->Username   = <Admin_Gmail_Id>;
+	$mail->Password   = <Gmail_Password>;
 
 	$mail->IsHTML(true);
-	//$mail->AddAddress("hemangee4700@gmail.com", "Hemangee De");
-	//$mail->SetFrom("dpkr5365@gmail.com", "Deepak Kumar");
-	$mail->AddAddress($to, "Deepak Kumar");
+	
+	$mail->AddAddress($to, <Admin_Name>);
 	$mail->SetFrom($email, $name);
-	//$mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
-	//$mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");
+	
 	$mail->Subject = $subject;
 	//$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
 	$content=$message;
