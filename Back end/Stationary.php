8babf5761153db389ac11a4c6f7bb400c44cd917
neha1609 <50171897+neha1609@users.noreply.github.com>
2021-02-10 15:50:59 +0530

    Comments added

--- Back end/Stationary.php
+++ Back end/Stationary.php
@@ -17,131 +17,59 @@ session_start();
   <title>Stationary</title>
 </head>
 
-<body>
-  <?php
-    include("includes/header.php");
-  ?>
-  
-  <section class="Books">
+  <body>
+    <?php
+      include("includes/header.php");
+    ?>
 
-<section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4">
-      
-        <?php
-          $get_products = "select * from products";
-          $run_products = mysqli_query($con,$get_products);
-          while ($row_product = mysqli_fetch_array($run_products)) {
-            $cat_id = $row_product['cat_id'];
-            if($cat_id==3) {
-              $pro_id=$row_product['product_id'];
-              $image = $row_product['product_img'];
-              $product_title = $row_product['product_title'];
-              $product_price = $row_product['product_price'];
-              ?>
-              <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a class="block relative h-48 rounded overflow-hidden" href="product.php?id=<?php echo $pro_id; ?>">
-                  <img alt="ecommerce" class="object-cover object-center w-40 h-70 block" src="image/<?php echo $image; ?>">
-                </a>
-                <div class="mt-4">
-                  <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">STATIONARY</h3>
-                  <h2 class="text-gray-900 title-font text-lg font-medium"><?php echo $product_title; ?></h2>
-                  <p class="mt-1">Rs. <?php echo $product_price; ?></p>
-                </div>
-              </div>
-              <?php
+    <section class="Books">
 
-            }
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-4">
 
-            
-          }
-        ?>
-        
-      <!--<div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261">
-        </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p class="mt-1">$21.15</p>
+            <?php
+              $get_products = "select * from products";
+              $run_products = mysqli_query($con,$get_products);
+              while ($row_product = mysqli_fetch_array($run_products)) {
+                $cat_id = $row_product['cat_id'];
+                if($cat_id==3) {
+                  $pro_id=$row_product['product_id'];
+                  $image = $row_product['product_img'];
+                  $product_title = $row_product['product_title'];
+                  $product_price = $row_product['product_price'];
+                  ?>
+                  <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden" href="product.php?id=<?php echo $pro_id; ?>">
+                      <img alt="ecommerce" class="object-cover object-center w-40 h-70 block" src="image/<?php echo $image; ?>">
+                    </a>
+                    <div class="mt-4">
+                      <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">STATIONARY</h3>
+                      <h2 class="text-gray-900 title-font text-lg font-medium"><?php echo $product_title; ?></h2>
+                      <p class="mt-1">Rs. <?php echo $product_price; ?></p>
+                    </div>
+                  </div>
+                  <?php
+
+                }
+
+              }
+            ?>
+
         </div>
       </div>
-      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262">
-        </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p class="mt-1">$12.00</p>
-        </div>
-      </div>
-      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263">
-        </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p class="mt-1">$18.40</p>
-        </div>
-      </div>
-      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264">
-        </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p class="mt-1">$16.00</p>
-        </div>
-      </div>
-      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265">
-        </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p class="mt-1">$21.15</p>
-        </div>
-      </div>
-      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267">
-        </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p class="mt-1">$12.00</p>
-        </div>
-      </div>
-      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268">
-        </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p class="mt-1">$18.40</p>
-        </div>
-      </div>-->
-    </div>
-  </div>
-</section>
+    </section>
 
 
-  </section>
+    </section>
 
-<!--  a new section here-->
-<?php
-  include("includes/footer.php");
+    <?php
+      include("includes/footer.php");
 
-?>
-  
+    ?>
+
     <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
-</body>
+  </body>
 </html>
 
 
