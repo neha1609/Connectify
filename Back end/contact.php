b361631e019c7ccd1edd82261ece863c4e564f75
Hemangee De <73026143+hemangeede@users.noreply.github.com>
2021-02-10 15:45:40 +0530

    Comments Added

--- Back end/contact.php
+++ Back end/contact.php
@@ -1,3 +1,4 @@
+<!--contact form-->
 <?php
   include("includes/dbcon.php");
   session_start();
@@ -21,34 +22,7 @@
   <?php
     include("includes/header.php");
   ?>
-  <!--<header class="text-gray-700 body-font">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span class="ml-3 text-xl">Connectify</span>
-      </a>
-      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <a class="mr-5 hover:text-gray-900" href="index.html">Home</a>
-        <a class="mr-5 hover:text-gray-900" href="About Us.html">About us</a>
-        <a class="mr-5 hover:text-gray-900" href="Books.html">Books</a>
-        <a class="mr-5 hover:text-gray-900" href="Stationary.html">Stationary</a>
-        <a class="mr-5 hover:text-gray-900" href="Study Material.html">Study Material</a>
-        <a class="mr-5 hover:text-gray-900" href="contact.html">Contact</a>
-
-
-
-      </nav>-->
-
-      <!-- <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Butto
-    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-      <path d="M5 12h14M12 5l7 7-7 7"></path>
-    </svg>
-  </button>
--->
-    <!--</div>
-  </header>-->
+  
   <!-- a new section here  -->
 
   <section class="text-gray-600 body-font relative">
@@ -89,7 +63,7 @@
           </div>
         </form>
           <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-            <a class="text-indigo-500">dpkr5365@gmail.com</a>
+            <a class="text-indigo-500"><Admin_Mail_Id></a>
             <p class="leading-normal my-5">Salt Lake
               <br>Kolkata, 700102
             </p>
@@ -125,21 +99,7 @@
   </section>
 
 
-<?php
-  /*if(isset($_POST['submit'])) {
-    $email = $_POST['email'];
-    $name = $_POST['name'];
-    $message = $_POST['message'];
-    $to      = 'nehajhawar1609@gmail.com';
-    $subject = 'Mail from '.$name;
-    $headers = 'From: '.$email . "\r\n" .
-        "CC: nehajhawar1609@gmail.com";
 
-    mail($to, $subject, $message, $headers);
-
-  }*/
-  
-?>
 
 
 <!--  a footer section here-->
